c13694e2abd56c152f6157888b0f06aae4ece147
Michael Cucina <97804565+mcucina1@users.noreply.github.com>
2023-02-15 17:23:39 -0700

    Add file

---
+++ webscraper.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+html_text = requests.get('https://app.joinhandshake.com/stu/postings?page=1&per_page=25&sort_direction=desc&sort_column=default&job.job_types%5B%5D=3').text
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('div', class_='style__cards___1xGnw')
+print(jobs)
+print(html_text)
+def find_jobs():
+    index = 1
+    with open('job_postings.txt', 'w') as file:
+        for job in jobs:
+            position = job.find('span', id="jobTitle-b57b5878c6a0d35a").text.replace(' ','')
+            file.write("Job #{index}\n")
+    #index = 1
+    #with open('job_postings.txt', 'w') as file:
+    #    for job in jobs:
+    #        published_date = job.find('span', class_='sim-posted').span.text
+    #        if 'few' in published_date:
+    #            company_name=job.find('h3', class_= 'joblist-comp-name').text.replace(' ','')
+    #            skills = job.find('span', class_= 'srp-skills').text.replace(' ', '')
+    #            link = job.header.h2.a['href']
+    #            file.write(f'Job Posting #{index}\n\n')
+    #            file.write(f'Company Name: {company_name.strip()}\n')
+    #            file.write(f'Skills: {skills.strip()}\n')
+    #            file.write(f'Link: {link}\n\n')
+    #            index+=1
+
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        print('Waiting 10 minutes...')
+        time.sleep(600)
